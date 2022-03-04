587742f8242a8c0c8874830203cbedb6f72bb748
MarkGarOliver <marcosgarcia.oliveira02@gmail.com>
2022-03-04 15:40:06 -0300

    upload arqs da page após login | validação com token

---
+++ logado/main.js
@@ -0,0 +1,20 @@
+'use strict'
+
+let titulo = document.getElementById('titulo')
+
+let usuarioLogado = JSON.parse(localStorage.getItem('usuarioLogado'))
+
+titulo.innerHTML = `Olá ${usuarioLogado.user}`
+
+if (localStorage.getItem('token') == null){
+    alert('VOCÊ PRECISA ESTAR LOGADO PARA ACESSAR ESSA PÁGINA')
+    window.location.href = 'http://127.0.0.1:5500/'
+
+    
+}
+
+function sair() {
+    localStorage.removeItem('usuarioLogado')
+    localStorage.removeItem('token')
+    window.location.href = 'http://127.0.0.1:5500/'
+}
